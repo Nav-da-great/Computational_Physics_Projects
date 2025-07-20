2610eccbe916758ff02f9f5e539c4fe2ce067246
Nav-da-great <113965179+Nav-da-great@users.noreply.github.com>
2025-07-20 17:26:30 +0530

    Update orbits.py
    Added exit button and cleaned up code

--- classical_mechanics/orbits.py
+++ classical_mechanics/orbits.py
@@ -1,29 +1,41 @@
 from vpython import *
+from tools import *
+
+# Simulation of a satellite in an orbit around the Earth
+
+canvas(title = "<h1>Angular momentum simulation</h1>", background = color.black)
+sim = Simulation()
 
 graph1 = graph(xtitle = 'time', ytitle = 'Energy')
 kinetic_energy = gcurve(color = color.red, label = 'Kinetic Energy')
 potential_energy = gcurve(color = color.blue, label = 'Potential Energy')
 total_energy = gcurve(color = color.yellow, label = 'Total Energy')
 
+# ========================================== Exit button section ========================================== #
+
+exit_button = button(bind = sim.exit, text = "Exit simulation", background = color.red, color = color.white)
+
+# ========================================== Physical simulation section ========================================== #
+
 G = 6.67e-11 # Universal constant of gravitation
 Me = 5.97e25 # Mass of Earth
 Re = 6.37e6 # Radius of the Earth
 
 earth = sphere(pos = vector(0, 0, 0), radius = Re, texture = textures.earth)
-satellite = sphere(pos = vector(2*Re, 0, 0), radius = Re/30, make_trail = True)
+satellite = sphere(pos = vector(2.5*Re, 0, 0), radius = Re/30, make_trail = True)
 
 satellite.mass = 1000
-
 vorbital = sqrt(G * Me / (2 * Re))
-
 satellite.momentum = vector(0, vorbital, 0) * satellite.mass
-print(vorbital)
+
+
+# ========================================== Animation loop ========================================== #
 
 t = 0
-dt = 10
+dt = 5
 
-while True:
-    rate(200)
+while sim.running:
+    rate(500)
 
     r = satellite.pos - earth.pos
 
