97ab945d2ed9f89a1f8f2664174292c9223d977b
Nav-da-great <113965179+Nav-da-great@users.noreply.github.com>
2025-08-06 22:49:37 +0530

    Update tools.py
    Added bind for pause

--- tools.py
+++ tools.py
@@ -1,12 +1,23 @@
 from vpython import *
 
-# This file contains all useful functions that can be alloted as a bind to various interactable classes in VPython
+# This file contains all useful functions that can be used as a bind to various interactable classes in VPython
 
-# Exit bind for button class
 class Simulation:
     def __init__(self):
         self.running = True # Creates a running instance
+        self.pause = True
 
-    def exit(self):
+    # Exit bind
+    def exit_bind(self):
         self.running = False
         print("User exit.")
+    
+    # Pause bind
+    def pause_bind(self, evt):
+        self.pause = not self.pause
+        if evt.text == "Pause": 
+            evt.text = "Play"
+            evt.background = color.green
+        else: 
+            evt.text = "Pause"
+            evt.background = color.blue
